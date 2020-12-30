c054f4c95e94288704c84fe2f86cbf3e1ce58f02
Jean Barcellos <jean.barcellos@hevo.com.br>
2020-12-29 23:00:50 -0300

    Adição do DataContext / EntityFrameworkCore

--- Startup.cs
+++ Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using testeef.Data;
 
 namespace testeef
 {
@@ -22,13 +24,21 @@ namespace testeef
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // Este método é chamado pelo tempo de execução. Use este método para adicionar serviços ao contêiner.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Registra o contexto fornecido como um serviço no IServiceCollection
+            services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
+
+            // Adiciona um serviço com escopo do tipo especificado em DataContext com um tipo de implementação especificado em DataContext para o IServiceCollection especificado.
+            services.AddScoped<DataContext, DataContext>();
+
+            // Adiciona serviços para controladores ao IServiceCollection especificado.
+            // Este método não registrará serviços usados para visualizações ou páginas.
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // Este método é chamado pelo tempo de execução. Use este método para configurar o pipeline de solicitação HTTP.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
